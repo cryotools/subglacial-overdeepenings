e1dc2c1e81fa2d776b26f84e07e5666cfafbe74a
WFurian <furiawil@geo.hu-berlin.de>
2020-11-20 08:50:48 +0100

    improved R script
    - added library(raster)
    - added explanations

--- code/regionGroup.R
+++ code/regionGroup.R
@@ -1,13 +1,17 @@
-input <- list.files(path = "~", # work_path from python script
+# Fast alternative to region group algorithm in arcpy
+
+library(raster)
+# load every single overdeepening in HMA
+input <- list.files(path = "~", # this path has to be the same as the work_path from the python script
                     pattern = "sink", recursive = TRUE, full.names = T)
-input <- input[!grepl('aux|vat|tfw|ovr|xml', input)]
+input <- input[!grepl('aux|vat|tfw|ovr|xml', input)] # grep only the .shp file of the overdeepenings
 
 fun_regionGroup <- function(input){
   print(input)
   name <- as.character(input)
   test <- raster(name)
-  regions <- clump(test) # alternative to region group routine from arcpy
-  out_path = paste(substr(input[1],1,29),"sinksRegion.tif", sep="")
+  regions <- clump(test) # region group
+  out_path = paste0(substr(input[1],1,29),"sinksRegion.tif")
   writeRaster(regions, out_path,format = "GTiff", overwrite = TRUE)
 }
 
